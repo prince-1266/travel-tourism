4e0f852ee4fe701096ef47c47549747a11f2338e
prince <modhprince12663515@gmail.com>
2025-11-21 18:48:07 +0530

    initial commit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
+  const fullname = name;
   try {
     let user = await User.findOne({ email });
     if (user) return res.status(400).json({ message: "User already exists" });
